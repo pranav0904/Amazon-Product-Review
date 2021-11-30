925d1e7adf78332e26a30cd7072438c6fe676e16
Pranav Khiste <pranavkhiste1@gmail.com>
2021-11-30 12:45:19 -0500

    Update settings.py
    Removed High Entropy Secret that exposed within the code

--- AmazonProductReviewScraping/reviews/settings.py
+++ AmazonProductReviewScraping/reviews/settings.py
@@ -14,7 +14,6 @@ accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/w
 accept-encoding: gzip, deflate, br
 accept-language: en-US,en;q=0.9
 cache-control: max-age=0
-cookie: session-id=143-5065477-6172528; session-id-time=2082787201l; i18n-prefs=USD; ubid-main=134-6673698-5515818; session-token=by0ChD+Nvqo3gjFTZrYrMTKIdp1jmsIfBF++8cRKs7W4vkmbxuPKmBWYNcn9XEtoP9Itd3kPgPs1i8bB55Ybk9eTDAgz1aM0CgvSLSDvRV8egsq/WuaLGwda3c1Fxuz6eGuSJDFWaIsYuVPyWjYMKg62Y7yLpE0ELCBJWzyWKYpXAYxgL4KumvA31eBN5vmm; csm-hit=tb:s-KM1MGBAJSXHB82XHA59Q|1638218147027&t:1638218150133&adb:adblk_no; lc-main=en_US
 downlink: 10
 ect: 4g
 rtt: 50
